36d3a3983de5680055f3a32aa2eb64c975296995
MLanky <lee.melzer@gmail.com>
2023-09-24 12:37:40 -0700

    added single post page

--- src/components/blogPosts/blogPosts/BlogPosts.js
+++ src/components/blogPosts/blogPosts/BlogPosts.js
@@ -2,13 +2,13 @@ import "./blogPosts.css"
 import Header from "../header/Header"
 import TopBar from "../topBar/TopBar"
 import Posts from "../posts/Posts"
+import SinglePost from "../../singlePost/SinglePost"
 
 export default function BlogPosts() {
   return (
     <>
-      <TopBar/>
-      <Header/>
-      <Posts/>
+    <TopBar/>
+    <SinglePost/>
     </>
     
   )
