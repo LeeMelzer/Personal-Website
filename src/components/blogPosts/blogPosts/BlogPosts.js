2f68ce6460bd37ae583b06a7dfb79ddc59f5c68b
MLanky <lee.melzer@gmail.com>
2023-09-27 10:28:51 -0700

    front end complete -> login and write pages added

--- src/components/blogPosts/blogPosts/BlogPosts.js
+++ src/components/blogPosts/blogPosts/BlogPosts.js
@@ -3,13 +3,14 @@ import Header from "../header/Header"
 import TopBar from "../topBar/TopBar"
 import Posts from "../posts/Posts"
 import SinglePost from "../../singlePost/SinglePost"
+import Login from "../../login/Login"
+import Write from "../../write/Write"
 
 export default function BlogPosts() {
   return (
     <>
     <TopBar/>
-    <SinglePost/>
+    <Write/>
     </>
-    
   )
 }
--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -2,6 +2,15 @@ import "./login.css"
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <div className="login">
+      <span className="loginTitle">Login</span>
+      <form className="loginForm">
+        <label>Username</label>
+        <input type="text" className="loginInput" placeholder="Enter your username..."/>
+        <label>Password</label>
+        <input type="password" className="loginInput" placeholder="Enter your password..."/>
+        <button className="loginButton" type="submit">Login</button>
+      </form>
+    </div>
   )
 }
