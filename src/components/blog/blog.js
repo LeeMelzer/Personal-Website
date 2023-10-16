6d41bbf27fa829ccc5285a05d376fffccd6d2814
MLanky <lee.melzer@gmail.com>
2023-10-16 12:18:04 -0700

    fully operational

--- src/components/blog/blog.js
+++ src/components/blog/blog.js
@@ -1,12 +1,28 @@
 import "./blog.css"
+import Post from "../blogPosts/post/Post"
+import { useEffect, useState } from "react"
+import axios from "axios"
 
 export default function Experience() {
+  const url = "http://localhost:5000/api/posts";
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async ()=>{
+      const res = await axios.get(url);
+      setPosts(res.data)
+    }
+    fetchPosts();
+  }, [])
 
   return (
     <section id="blog">
       <h5>Follow My Journey</h5>
       <h2>Blog</h2>
-      <p className="message">How in the hell do I make this work???</p>
+      <div className="posts">{posts.map(p => <Post post={p}/>)}
+      <a href="http://localhost:3000/blogPosts" className="btn btn-primary">See Blog Posts</a>
+      </div>
+      
     </section>
   )
 }
